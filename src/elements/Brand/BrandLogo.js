efd20065da6649c53add8ee46f20953a9ab93ab7
unknown <rizky.darmarazak@gmail.com>
2021-08-20 09:39:49 +0700

    Element Brand Logo 20 aug 2021

---
+++ src/elements/Brand/BrandLogo.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import LogoWhite from 'assets/images/logo_rosid_white.png';
+import LogoRed from 'assets/images/logo_rosid_red.png';
+import './BrandLogo.scss';
+
+export default function BrandLogo({ color, size }) {
+  const classes = [color, size];
+
+  return (
+    <div className={`brand_logo ${classes.join(' ')}`}>
+      <figure className={`brand_image ${classes.join(' ')}`}>
+        <img
+          src={classes === 'white' ? LogoWhite : LogoRed}
+          alt="logo rosid warna putih"
+          className="img-cover"
+        />
+      </figure>
+      <span className={`brand_name ${classes.join(' ')}`}>StudioRosid</span>
+    </div>
+  );
+}
+
+BrandLogo.propTypes = {
+  color: propTypes.string,
+  size: propTypes.string,
+};
